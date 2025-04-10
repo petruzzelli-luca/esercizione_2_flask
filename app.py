c052ec33a4753bf0e316afa078b3d66dd9dc5277
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-04-10 10:33:13 +0000

    secondo commit

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 import pandas
-from flask import Flask, render_template, request #importiamo la classe flask
+from flask import Flask, render_template, redirect, url_for, request #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
 
-df = pandas.read_csv("profile.csv")
-user = df.iloc[0].to_dict()
+
 @app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
 def home():
     df = pandas.read_csv("profile.csv")
-    user = df.iloc[0].to_dict()
-    return render_template("index.html",df=user)
+    dati = df.iloc[0].to_dict()
+    print(dati)
+    return render_template("index.html",df=dati)
 
-@app.route('/modifica_profilo', methods=['GET', 'POST'])  # aggiungi GET per il rendering
-def modifica_profilo():
-    return render_template("modifica.html")
+@app.route("/modifica", methods=["GET", "POST"])
+def modifica():
+    if request.method == "POST":
+        nuovo_profilo = {
+            "Nome": request.form["nome"],
+            "Cognome": request.form["cognome"],
+            "Scuola": request.form["scuola"],
+            "Hobby": request.form["hobby"]
+        }
+        df = pandas.DataFrame([nuovo_profilo])
+        df.to_csv("profile.csv", index=False)
+        return redirect(url_for('home'))
+    df = pandas.read_csv("profile.csv")
+    dati = df.iloc[0].to_dict()
+    return render_template("modifica.html", dati=dati)
 
-@app.route('/salva', methods=['GET', 'POST'])  # aggiungi GET per il rendering
-def salva():
-    nome_nuovo = request.form['nome']
-    cognome_nuovo = request.form['cognome']
-    user_nuovo = user.update({'Nome': nome_nuovo, 'Cognome': cognome_nuovo})
+
 
 #avvio flask
 if __name__ == '__main__':
